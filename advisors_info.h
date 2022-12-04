c12c8d4608c6b820e4e21bca84ec318f7411549d
jogata5 <ogatajulian@gmail.com>
2022-12-03 21:42:08 -0800

    Added license to each file

--- advisors_info.h
+++ advisors_info.h
@@ -1,3 +1,28 @@
+//    This program is called Virtual Advisor and aims to provide CSUF Students with
+//    an easier method to figure out what classes they should take next. The
+//    user will input their information and pick the classes that they have completed.
+//    The program will output a list of classes the user can take next.
+
+//    Creators:
+//      Julian Ogata    - jogata@csu.fullerton.edu
+//      Dilhan Franco   - dilhanfranco@csu.fullerton.edu
+//      Parth Sharma    - parthsharma@csu.fullerton.edu
+
+//    Copyright (C) 2022  Julian Ogata, Dilhan Franco, Parth Sharma
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #ifndef ADVISORS_INFO_H
 #define ADVISORS_INFO_H
 
